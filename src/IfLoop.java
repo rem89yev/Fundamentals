84a145257593e9e153e16643ec5cb0f3e005bf3f
rem89yev <rem89yev@gmail.com>
2017-01-21 20:02:07 +0200

    Initial commit

---
+++ src/IfLoop.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Yevhen-PC on 21.01.2017.
+ */
+public class IfLoop {
+
+    public static void main(String[] args) {
+
+        int myInt = 5;
+        if (myInt < 10) {
+
+            System.out.println("Да, верно");
+
+        } else if (myInt < 20) {
+
+            System.out.println("Нет, не верно");
+
+        }
+    }
+}
+
