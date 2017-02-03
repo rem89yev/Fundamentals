818bc74ac12ef408e914a0e3f74171a3504eb820
rem89yev <rem89yev@gmail.com>
2017-02-03 22:18:07 +0200

    Initial commit

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -8,12 +8,18 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Роман";
         person1.age = 50;
-        person1.speak();
+        //person1.speak();
 
         Person person2 = new Person();
         person2.name = "Семен";
         person2.age = 18;
-        person2.sayHello();
+        //person2.sayHello();
+        //person1.calculateYearsToRetirement();
+        //person2.calculateYearsToRetirement();
+        int year1 = person1.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии " + year1 + " лет");
+        System.out.println("Второму человеку до пенсии " + year2 + " лет");
     }
 }
 
@@ -24,6 +30,19 @@ class Person {
 
     String name;
     int age;
+
+    //void calculateYearsToRetirement() {
+    //    int years = 65-age;
+    //System.out.println("Количество лет до пенсии: " + years);}
+
+
+
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
+        return years;
+
+    }
+
     void speak() {
         for(int i = 0; i<3; i++) {
 
