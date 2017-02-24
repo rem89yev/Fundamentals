ae5c8df1a628763882f29a11cfe5e317d9b6338f
rem89yev <rem89yev@gmail.com>
2017-02-24 23:09:27 +0200

    Initial commit

---
+++ src/prometeus/SquareRoot.java
@@ -0,0 +1,23 @@
+package prometeus;
+
+/**
+ * Created by Yevhen-PC on 24.02.2017.
+ */
+public class SquareRoot {
+
+        public static void main(String[] args) {
+            double a = 3;
+            double b = 2.5;
+            double c = -0.5;
+
+            double x1 = Math.sqrt(a);
+            double x2 = Math.sqrt(b);
+            double x3 = Math.sqrt(c);
+
+            System.out.println("x1" + "=" + x1);
+            System.out.println("x2" + "=" + x2);
+            System.out.println("x3" + "=" + x3);
+        }
+}
+
+
